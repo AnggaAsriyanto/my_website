d45ef5b273090a4c152596623cee730946793f73
AnggaNellow <anggazxcasd1@gmail.com>
2021-06-25 02:20:55 +0700

    deploy my website 0.0

---
+++ app.js
@@ -0,0 +1,23 @@
+const express     = require('express');
+const app         = express();
+const bodyParser  = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/projects', (req, res) => {
+    res.render('projects')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.listen(3000, () => console.log('Server has started on port 3000!'));
+
